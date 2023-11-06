9609cd3140ac7557e4724f68f429637db9907136
Sungjae Shin <67855888+SungJaeShin@users.noreply.github.com>
2023-11-07 02:28:05 +0900

    Compute Parallax

---
+++ src/outlier_rejection.h
@@ -0,0 +1,68 @@
+#include <opencv2/opencv.hpp>
+#include "opencv2/xfeatures2d.hpp"
+
+cv::Mat outlierRejection(std::vector<cv::Point2f> &img1_2d, std::vector<cv::Point2f> &img2_2d, 
+                         std::vector<cv::KeyPoint> img1_kpt, std::vector<cv::KeyPoint> img2_kpt, 
+                         std::vector<std::vector<cv::DMatch>> matches, double ransacReprojThreshold, double confidence,
+                         std::vector<cv::KeyPoint> &final_query_kpt, std::vector<cv::KeyPoint> &final_cand_kpt,
+                         std::vector<cv::DMatch> &final_matches)
+{
+    double threshold = 0.95f;
+    std::vector<cv::KeyPoint> good_query_kpt, good_cand_kpt; 
+	std::vector<cv::DMatch> good_matches;
+
+    for(int i = 0; i < matches.size(); i ++)
+    {
+        if(matches[i][0].distance < threshold * matches[i][1].distance)
+        {
+            int query_idx = matches[i][0].queryIdx;
+            int cand_idx = matches[i][0].trainIdx;
+            img1_2d.push_back(img1_kpt[query_idx].pt);
+            img2_2d.push_back(img2_kpt[cand_idx].pt);
+            good_query_kpt.push_back(img1_kpt[query_idx]);
+            good_cand_kpt.push_back(img2_kpt[cand_idx]);
+            good_matches.push_back(matches[i][0]);
+        }
+    }
+
+    // Get final correspondence pair     
+    cv::Mat F;
+    std::vector<uchar> status;
+    F = cv::findFundamentalMat(img1_2d, img2_2d, cv::FM_RANSAC, ransacReprojThreshold, confidence, status);
+
+    for(int i = 0; i < img1_2d.size(); i++)
+    {
+        cv::Mat query_2pt = (cv::Mat_<double>(3, 1) << img1_2d[i].x, img1_2d[i].y, 1);
+        cv::Mat cand_2pt = (cv::Mat_<double>(1, 3) << img2_2d[i].x, img2_2d[i].y, 1);
+        if(status[i] > 0)
+        {
+            // std::cout << "[inlier] x'Fx = " << cand_2pt * F * query_2pt << std::endl;
+            final_query_kpt.push_back(good_query_kpt[i]);
+            final_cand_kpt.push_back(good_cand_kpt[i]);
+            final_matches.push_back(good_matches[i]);
+        }
+    }
+
+    return F;
+}
+
+void outlierParallax(double avg_dx, double avg_dy,
+                     std::vector<cv::KeyPoint> query_kpt, std::vector<cv::KeyPoint> cand_kpt, std::vector<cv::DMatch> matches,
+                     std::vector<cv::KeyPoint> &final_query_kpt, std::vector<cv::KeyPoint> &final_cand_kpt, std::vector<cv::DMatch> &final_matches)
+{
+    for(size_t i = 0; i < matches.size(); i++)
+    {
+        double dx = std::fabs(query_kpt[i].pt.x - cand_kpt[i].pt.x);
+        double dy = std::fabs(query_kpt[i].pt.y - cand_kpt[i].pt.y);
+
+        if(dx < avg_dx - 10 || dx > avg_dx + 10)
+            continue;
+        if(dy < avg_dy - 10 || dy > avg_dy + 10)
+            continue;
+
+        final_query_kpt.push_back(query_kpt[i]);
+        final_cand_kpt.push_back(cand_kpt[i]);
+        final_matches.push_back(matches[i]);
+    }
+}
+                         
---
+++ src/parallax.cpp
@@ -0,0 +1,88 @@
+#include <eigen3/Eigen/Dense>
+#include <list>
+#include <algorithm>
+#include <vector>
+#include <numeric>
+#include <iostream>
+#include <fstream>
+#include <opencv2/opencv.hpp>
+
+#include "extractor.h"
+#include "descriptor.h"
+#include "outlier_rejection.h"
+#include "parallax.h"
+
+void drawimage(cv::Mat image1, std::vector<cv::KeyPoint> img1_kpt)
+{
+    // Draw image keypoints 
+    cv::Mat outImg;
+    cv::drawKeypoints(image1, img1_kpt, outImg);
+    cv::imshow("Output", outImg);
+    cv::waitKey(0);
+}
+
+int main()
+{
+    // Road images
+    std::string img1_path = "/home/sj/workspace/paper_ws/covins_up/src/parallax/img/cand_1_3.png";
+    std::string img2_path = "/home/sj/workspace/paper_ws/covins_up/src/parallax/img/cand_1_9.png";
+    cv::Mat img1 = cv::imread(img1_path, 1);
+    cv::Mat img2 = cv::imread(img2_path, 2);
+
+    // Initialization keypoints and descriptor 
+    std::vector<cv::KeyPoint> img1_kpt, img2_kpt;
+    std::vector<cv::Point2f> img1_2d, img2_2d;
+    cv::Mat img1_des, img2_des;
+
+    // Extract features and descriptors
+    sift(img1, img1_kpt);
+    sift(img2, img2_kpt);
+    surf(img1, img2, img1_kpt, img2_kpt, img1_des, img2_des);
+
+    // Feature correspondence using features and descriptors
+    cv::Ptr<cv::DescriptorMatcher> matcher = cv::BFMatcher::create(cv::NORM_L2, false);
+    std::vector<std::vector<cv::DMatch>> matches;
+    matcher -> knnMatch(img1_des, img2_des, matches, 2);
+
+    // Final correspondence pair
+    cv::Mat F;
+    std::vector<cv::KeyPoint> final_query_kpt, final_cand_kpt;
+    std::vector<cv::DMatch> final_matches;
+    F = outlierRejection(img1_2d, img2_2d, img1_kpt, img2_kpt, matches, 1, 0.999, 
+                         final_query_kpt, final_cand_kpt, final_matches);
+
+    // Compute Parallax
+    double avg_dx = 0;
+    double avg_dy = 0;
+    double avg_dist = 0;
+    double max_dx = 0;
+    double max_dy = 0;
+    double max_dist = 0;
+    computeParallax(avg_dx, avg_dy, avg_dist, max_dx, max_dy, max_dist, 
+                    final_query_kpt, final_cand_kpt, final_matches);
+
+    // Final debug    
+    std::cout << "final correspondence pair: " << final_matches.size() << std::endl;
+    std::cout << "[After Filtering]" << std::endl;
+    std::cout << "max dx: " << max_dx << ", max dy: " << max_dy << ", max dist: " << max_dist << std::endl;
+    std::cout << "avg dx: " << avg_dx << ", avg dy: " << avg_dy << ", avg_dist: " << avg_dist << std::endl;
+
+
+    std::vector<cv::KeyPoint> filter_query_kpt, filter_cand_kpt;
+    std::vector<cv::DMatch> filter_matches;
+    outlierParallax(avg_dx, avg_dy, final_query_kpt, final_cand_kpt, final_matches,
+                    filter_query_kpt, filter_cand_kpt, filter_matches);
+    
+    std::cout << "final filtered correspondence pair: " << filter_matches.size() << std::endl;
+
+
+    // Final Img 
+    cv::Mat outImg;
+    cv::drawMatches(img1, img1_kpt, img2, img2_kpt, filter_matches, outImg);
+    cv::imshow("final correspondence", outImg);
+    cv::waitKey(0);
+
+    return 0;
+}
+
+
