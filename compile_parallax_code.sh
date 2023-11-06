1bd738d289430f6f7a68303a2384a1016392a749
Sungjae Shin <67855888+SungJaeShin@users.noreply.github.com>
2023-11-07 02:29:05 +0900

    Compute Parallax

--- compile_parallax_code.sh
+++ compile_parallax_code.sh
@@ -1,2 +1,2 @@
-g++ parallax.cpp -L/usr/local/include/opencv2 -lopencv_highgui -lopencv_imgcodecs -lopencv_imgproc -lopencv_core -lopencv_stitching -lopencv_features2d -lopencv_xfeatures2d -lopencv_flann -lopencv_calib3d -o parallax
+g++ ./src/parallax.cpp -L/usr/local/include/opencv2 -lopencv_highgui -lopencv_imgcodecs -lopencv_imgproc -lopencv_core -lopencv_stitching -lopencv_features2d -lopencv_xfeatures2d -lopencv_flann -lopencv_calib3d -o parallax
 ./parallax
